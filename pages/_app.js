c3ed4653482a715a44dd9047f5a89d1ae4e0da62
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-11-09 20:04:17 +0000

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout'
 import '../styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
